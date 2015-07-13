53c08ff916f5793f2ae3181a28f519f52f1b33a9
jonseijo <jon.seijo@gmail.com>
2015-07-12 21:12:44 -0300

    solve problem 2

--- problem_2.py
+++ problem_2.py
@@ -6,7 +6,9 @@ the Fibonacci sequence whose values
 do not exceed four million, 
 find the sum of the even-valued terms. """
 
-def fibonac(maxnumber):
+MAX_NUMBER = 4000000
+
+def getFibonac(maxnumber):
 	"""Returns a list with the Fibonacci numbers
 	smaller than  'maxnumber'
 	"""
@@ -15,8 +17,10 @@ def fibonac(maxnumber):
 	reached_max = False
 
 	while(not reached_max):
-		next_fib= sequence[i] + sequence[i-1]
+		#Number will be current index plus 
+		next_fib = sequence[i] + sequence[i-1]
 
+		#Stop adding if the next fibbonacci number is greater than the max
 		if next_fib >= maxnumber: 
 			reached_max = True;
 		else:
@@ -26,8 +30,25 @@ def fibonac(maxnumber):
 
 	return sequence
 
+def getEvenNumbers(number_list):
+	"""Takes a list of numbers and 
+	returns a list with the even"""
+
+	even_numbers = []
+	for n in number_list:
+		if n % 2 == 0:
+			even_numbers.append(n)
+
+	return even_numbers
+
+
 def main():
-	print fibonac(20)
+	sum_of_evens = 0
+	for n in getEvenNumbers(getFibonac(MAX_NUMBER)):
+		sum_of_evens += n
+
+	print sum_of_evens
+
 
 if __name__ == "__main__":
     main()
