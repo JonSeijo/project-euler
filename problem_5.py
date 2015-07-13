1b0d361fb07bfd780a72700942385ef8b82a3b30
Jonathan Seijo <jon.seijo@gmail.com>
2015-07-13 18:31:55 -0300

    update comments

--- problem_5.py
+++ problem_5.py
@@ -11,27 +11,6 @@ What is the smallest positive number t
 hat is evenly divisible by all of the numbers from 1 to 20?
 """
 
-"""
-BRUTE FORCE
-tomar n
-n % 1 == 0?
-n % 2 == 0?
-n % 3 == 0?
-..
-
-NO --> probar n+1
-SI --> answer = n 
-
-
-NOTE:
-No hay necesidad de probar todos los numeros del 1 al 20
-divisible por 2 => divisible por 2,4,6,8,10..
-divisible por 3 => divisible por 3,6,9,12,15..
-divisible por 5 => divisible por 5,10,15..
-
-===> Debe ser divisible por los primos menores a 20
-"""
-
 
 def main():
     greaterDivisor = 20
