0b45ba72992d35a73e7484b4be4a097480cdb5a2
jonseijo <jon.seijo@gmail.com>
2015-07-22 11:33:10 -0300

    Fix important bug with tens. Solve problem 17

--- problems 10-19/problem_17.py
+++ problems 10-19/problem_17.py
@@ -17,39 +17,65 @@ The use of "and" when writing out numbers is in compliance with British usage.
 
 units = ["", "one", "two", "three", "four", "five",
              "six", "seven", "eight", "nine"]
+specialTens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+               "sixteen", "seventeen", "eighteen", "nineteen"]
 tens = ["", "ten", "twenty", "thirty", "forty", "fifty",
             "sixty", "seventy", "eighty", "ninety"]
 
 def getWrittenNumber(number):
     """returns a string with the wirtten number.
     Max number == 999"""
-    
+
     numberStr = str(number)
     digits = len(numberStr)
 
     unit = units[int(numberStr[-1])]
+
     if digits >= 2:
-        ten = tens[int(numberStr[-2])]
+        # Si la decena empieza en 1
+        if(numberStr[-2] == "1"):
+            unit = units[0]
+            # The value of the ten name changes depending the 'one' digit
+            ten = specialTens[int(numberStr[-1])]
+        else:
+            ten = tens[int(numberStr[-2])]
+
     if digits >= 3:
+        # name of hundreds is the name of the unit with a 'hundred' at the end
         hundred = units[int(numberStr[-3])] + " hundred"
 
+    # Everything returns spaces so it can be more readable for debugging
+
     if digits == 1:
-        return unit
+        return unit + " "
 
     if digits == 2:
-        return ten + " " + unit
+        return ten + " " + unit + " "
 
     if digits == 3:
         if ten == "" and unit == "":
-            return hundred
+            return hundred + " "
         else:
-            return hundred + " and " + ten + " " + unit
+            return hundred + " and " + ten + " " + unit + " "
 
 
 def main():
-    wirttenNumber = getWrittenNumber(456)
-    print wirttenNumber
+    allNumbers = ""
+    charCount = 0
 
+    for n in range(1, 1000):
+        allNumbers += getWrittenNumber(n)
+    # 1000 is not included
+    allNumbers += "one thousand"
+
+    print allNumbers
+
+    for c in allNumbers:
+        if c != " ":
+            charCount += 1
+
+    # The answer is the sum of all chars in the written numbers
+    print charCount
 
 if __name__ == "__main__":
     main()
