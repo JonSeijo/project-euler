10c6d8fcdc6802d856cd13bdc0d2c88c501d14bf
jonseijo <jon.seijo@gmail.com>
2015-09-02 13:47:02 -0300

    Solve problem 51

--- problems 50-59/problem_51.py
+++ problems 50-59/problem_51.py
@@ -43,6 +43,9 @@ def getPrimes(maxNumber):
 
 
 def isPrime(number):
+    #print number
+
+    number = int(number)
     # Divisible by 2, not prime
     if number % 2 == 0:
         return False
@@ -74,12 +77,89 @@ def getBinaryPermutations(digits):
 
     return binaryPermutations
 
+
 def main():
-    # primes = getPrimes(100000)
+    """
+    Jonathan Seijo
 
-    print getBinaryPermutations(4)
+    Solves in about 40 seconds
+    --------
+    Clarifications (learned the hard way)
+    LEADING ZEROES DO NOT COUNT
+    thats why ***109 is not the answer.
 
+    The answer is NOT the smalest prime,
+    the answer is the smalles IN THE FAMILY OF *******
+    --------
 
+    The permutations of the changin digit (*)
+    ------->
+                *---
+                -*--
+                --*-
+                ---*
+                **--
+                *-*-
+                ...
+
+    are calculated using binaty numbers, with (1) representing (*)
+
+    ex      ---* = 0001
+            --*- = 0010
+
+            **** = 1111
+
+    so when there is a (1) in the binary, I replace the digits there
+
+    """
+    primes = getPrimes(300000)
+    permutations = getBinaryPermutations(len(str(primes[0])))
+
+    permutationDigits = 0
+
+    numberModify = ''
+
+    answer = 0
+
+    for number in primes:
+        if answer != 0:
+            break
+
+        # The binarylist (posible permutations of *)
+        # changes with respect the size of the number
+        if(len(str(number)) > permutationDigits):
+            permutations = getBinaryPermutations(len(str(number)))
+            permutationDigits = len(str(number))
+
+        # Every posible permutation of the digit
+        for permut in permutations:
+
+            numberModifyList = []
+
+            for digitChange in range(0, 10):
+
+                numberModify = str(number)
+                leadingZero = False
+
+                # to replace each 1 with the digit
+                for i in range(0, len(permut)):
+                    if permut[i] == '1':
+                        if (i == 0) and (digitChange == 0):
+                            leadingZero = True
+                            continue
+                        else:
+                            # strings are inmutable, i cant replace chars
+                            numberModify = numberModify[:i] + str(digitChange) + numberModify[i+1:]
+
+                if isPrime(numberModify) and not leadingZero:
+                    numberModifyList.append(numberModify)
+
+            if len(numberModifyList) == 8:
+                answer = min(numberModifyList)
+                print answer
+                break
+
+    print "answer: " + str(answer)
 
 if __name__ == "__main__":
     main()
